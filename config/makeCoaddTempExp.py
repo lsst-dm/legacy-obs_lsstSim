afb36afba64a9a15a86407a28dafc0750e442d6c
Yusra AlSayyad <yusra@astro.washington.edu>
2013-03-23 11:29:14 -0500

    clean up config files. Should not retarget selectFluxMag in makeCoaddTempExp until 2584

--- config/makeCoaddTempExp.py
+++ config/makeCoaddTempExp.py
@@ -1,5 +1,3 @@
 from lsst.obs.lsstSim.selectLsstImages import SelectLsstImagesTask
-from lsst.obs.lsstSim.scaleLsstSimZeroPoint import ScaleLsstSimZeroPointTask
 
 root.select.retarget(SelectLsstImagesTask)
-root.scaleZeroPoint.retarget(ScaleLsstSimZeroPointTask)
