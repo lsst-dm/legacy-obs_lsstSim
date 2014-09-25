c23792f3a4f326c6c39769d4132e029d3cc4005b
Simon Krughoff <simon.krughoff@gmail.com>
2014-09-24 21:45:54 -0700

    Added code to display a list of CCDs

--- bin/displayCamera.py
+++ bin/displayCamera.py
@@ -8,6 +8,10 @@ def checkStr(strVal, level):
         matchStr = '^R:[0-9],[0-9] S:[0-9],[0-9] A:[0-9],[0-9]$'
         if not re.match(matchStr, strVal):
             raise ValueError("Specify raft, ccd, and amp: %s"%(strVal))
+    elif level == 'ccd':
+        matchStr = '^R:[0-9],[0-9] S:[0-9],[0-9]$'
+        if not re.match(matchStr, strVal):
+            raise ValueError("Specify only raft and ccd: %s"%(strVal))
     else:
         raise ValueError("level must be one of: ('amp', 'ccd', 'raft')")
     return True
@@ -18,6 +22,9 @@ if __name__ == "__main__":
     parser.add_argument('--showAmp', help='Show an amplifier segment in ds9  May occur multiple times. '\
                                           'Format like R:Rx,Ry S:Sx,Sy A:Ax,Ay e.g. '\
                                           '\"R:2,2 S:1,1 A:0,0\"', type=str, nargs='+')
+    parser.add_argument('--showCcd', help='Show a CCD from the mosaic in ds9.  May occur multiple times. '\
+                                          'Format like R:Rx,Ry S:Sx,Sy e.g. \"R:2,2 S:1,1\"', type=str,
+                                          nargs='+')
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
@@ -35,4 +42,12 @@ if __name__ == "__main__":
                 detector = camera[raft+" "+ccd]
                 amplifier = detector[amp[2:]]
                 cameraGeomUtils.showAmp(amplifier, frame=frame)
+                frame += 1
+
+    if args.showCcd:
+        for ccdStr in args.showCcd:
+            if checkStr(ccdStr, 'ccd'):
+                raft, ccd = ccdStr.split()
+                detector = camera[raft+" "+ccd]
+                cameraGeomUtils.showCcd(detector, frame=frame)
                 frame += 1
