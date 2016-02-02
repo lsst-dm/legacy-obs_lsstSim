56d42bb7b9b14a3e480ef452b05f042a44fe37d9
Russell Owen <rowen@uw.edu>
2016-02-02 15:54:00 -0800

    Updated config/processCcd.py

--- config/processCcd.py
+++ config/processCcd.py
@@ -6,8 +6,8 @@ config.isr.retarget(LsstSimIsrTask)
 
 try:
     import lsst.meas.extensions.multiShapelet
-    config.measurement.algorithms.names = set(config.measurement.algorithms.names) | lsst.meas.extensions.multiShapelet.algorithms
-    config.measurement.slots.modelFlux = "multishapelet.combo.flux"
+    config.calibrate.detectAndMeasure.measurement.algorithms.names = set(config.measurement.algorithms.names) | lsst.meas.extensions.multiShapelet.algorithms
+    config.calibrate.detectAndMeasure.measurement.slots.modelFlux = "multishapelet.combo.flux"
 except ImportError:
     # TODO: find a better way to log this
     print "WARNING: Could not import lsst.meas.extensions.multiShapelet; model fluxes not enabled!"
