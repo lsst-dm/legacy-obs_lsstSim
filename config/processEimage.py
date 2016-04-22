a8438382c13fcb8f0dce3df8fa8743ca02dff50b
Simon Krughoff <simon.krughoff@gmail.com>
2016-04-22 10:05:42 -0700

    Update config for u-band

--- config/processEimage.py
+++ config/processEimage.py
@@ -1,1 +1,5 @@
 config.calibrate.photoCal.doSelectUnresolved=False
+
+# Allows u-band to lock on to correct locus
+# Does not seem to hurt r-band data
+config.charImage.measurePsf.starSelector.widthStdAllowed=1.
