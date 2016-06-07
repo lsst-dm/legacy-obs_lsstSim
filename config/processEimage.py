e4b1c5d639b953bafab6ce67fa428885cd4f6b24
Russell Owen <rowen@uw.edu>
2016-06-07 11:24:51 -0700

    Update config override for star selector in a registry

--- config/processEimage.py
+++ config/processEimage.py
@@ -2,4 +2,4 @@ config.calibrate.photoCal.doSelectUnresolved=False
 
 # Allows u-band to lock on to correct locus
 # Does not seem to hurt r-band data
-config.charImage.measurePsf.starSelector.widthStdAllowed=1.
+config.charImage.measurePsf.starSelector["objectSize"].widthStdAllowed=1.
