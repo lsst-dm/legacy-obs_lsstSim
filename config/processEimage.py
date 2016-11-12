44f6f573ea05385b9e24a08cc6d58da16115afe5
Simon Krughoff <krughoff@uw.edu>
2016-11-12 14:30:31 -0800

    Configs and images selection for homogenized coadds

--- config/processEimage.py
+++ config/processEimage.py
@@ -1,5 +1,13 @@
+config.charImage.repair.doCosmicRay=True
+
 config.calibrate.photoCal.doSelectUnresolved = False
+config.charImage.installSimplePsf.fwhm=2.
 
 # Allows u-band to lock on to correct locus
 # Does not seem to hurt r-band data
 config.charImage.measurePsf.starSelector["objectSize"].widthStdAllowed = 1.
+
+config.charImage.measurePsf.psfDeterminer.name = "pca"
+config.charImage.measurePsf.psfDeterminer['pca'].kernelSize=25.
+config.charImage.measurePsf.psfDeterminer['pca'].kernelSizeMax=25
+config.charImage.measurePsf.psfDeterminer['pca'].kernelSizeMin=25
