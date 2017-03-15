b051dae843de81d53a876bb433f4215eb8f81fe2
Simon Krughoff <simon.krughoff@gmail.com>
2017-03-15 11:28:47 -0700

    Use same overrides as obs_subaru

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -12,4 +12,4 @@ config.measurement.slots.modelFlux = 'modelfit_CModel'
 
 config.measurement.load(os.path.join(getPackageDir("meas_extensions_shapeHSM"), "config", "enable.py"))
 config.measurement.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
-#config.catalogCalculation.plugins['base_ClassificationExtendedness'].fluxRatio = 0.985
+config.catalogCalculation.plugins['base_ClassificationExtendedness'].fluxRatio = 0.985
