ac81e88fb5a0eaf37c255abf6aa45cc2580b879c
Perry Gee <pgee2000@gmail.com>
2017-09-08 16:20:54 -0700

    Set config files to retain defaults prior to DM-11521

--- config/processCcd.py
+++ config/processCcd.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 from lsst.obs.lsstSim import LsstSimIsrTask
 
 config.isr.retarget(LsstSimIsrTask)
+# this was the default prior to DM-11521.  New default is 2000.
+config.calibrate.deblend.maxFootprintSize=0
 
 try:
     import lsst.meas.extensions.multiShapelet
