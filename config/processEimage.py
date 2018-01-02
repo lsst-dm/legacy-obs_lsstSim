47f249f64d085e237947cebf9a9899fcf8a8692c
Ian Sullivan <ianssullivan@gmail.com>
2018-01-02 11:19:22 -0800

    Fix typo in call to sourceSelection in processEimage.py

--- config/processEimage.py
+++ config/processEimage.py
@@ -1,4 +1,4 @@
-config.calibrate.photoCal.sourceSelection.doUnresolved = False
+config.calibrate.photoCal.match.sourceSelection.doUnresolved = False
 
 # Allows u-band to lock on to correct locus
 # Does not seem to hurt r-band data
