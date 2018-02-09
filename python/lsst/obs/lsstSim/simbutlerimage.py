e79d58901ccb59470a5dc3309a737f89557153aa
Simon Krughoff <krughoff@lsst.org>
2018-02-09 14:20:22 -0600

    Support subsampled sensors

--- python/lsst/obs/lsstSim/simbutlerimage.py
+++ python/lsst/obs/lsstSim/simbutlerimage.py
@@ -1,9 +1,12 @@
 from lsst.afw.cameraGeom.utils import ButlerImage, calcRawCcdBBox
 import lsst.afw.image as afwImage
+import lsst.afw.math as afwMath
 from lsst.afw.math import rotateImageBy90
+from lsst.daf.persistence.butlerExceptions import NoResults
 
 
 class SimButlerImage(ButlerImage):
+
     def getCcdImage(self, ccd, imageFactory=afwImage.ImageF, binSize=1):
         """Return an image of the specified ccd, and also the (possibly updated) ccd"""
 
@@ -21,13 +24,18 @@ class SimButlerImage(ButlerImage):
             im = None
             cid = ccd.getName()
             did = parse_name_to_dataId(cid)
+            # TODO override the constructor to handle this as state
+            sensor_bin_size = self.kwargs.pop('sensorBinSize')
             self.kwargs.update(did)
             try:
                 im = self.butler.get(self.type, **self.kwargs)
                 ccd = im.getDetector()  # possibly modified by assembleCcdTask
                 im.setMaskedImage(rotateImageBy90(im.getMaskedImage(), 2))
-            except Exception:
+                binned_im = afwMath.binImage(im.getMaskedImage(), sensor_bin_size)
+                self.butler.put(binned_im, 'binned_sensor_fits', **self.kwargs)
+            except (NoResults, RuntimeError):
                 pass
+            self.kwargs['sensorBinSize'] = sensor_bin_size
 
 
         if im is None:
